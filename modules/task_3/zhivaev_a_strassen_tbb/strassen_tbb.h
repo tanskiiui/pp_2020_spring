f8d2afa20a35a18058907277480150af58abf4ab
tanskiiui <yurko1337@gmail.com>
2020-05-04 22:26:20 +0300

    test1

--- modules/task_3/zhivaev_a_strassen_tbb/strassen_tbb.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2020 Zhivaev Artem
-
-#ifndef MODULES_TASK_3_ZHIVAEV_A_STRASSEN_TBB_STRASSEN_TBB_H_
-#define MODULES_TASK_3_ZHIVAEV_A_STRASSEN_TBB_STRASSEN_TBB_H_
-
-void multStrassenTbb(int size, const double* a, const double* b, double* result);
-
-#endif  // MODULES_TASK_3_ZHIVAEV_A_STRASSEN_TBB_STRASSEN_TBB_H_
